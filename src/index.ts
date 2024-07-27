2e04d94ba7b234eb8a26e314128712921988f328
Riki Khorana <riki.singh.khorana@sony.com>
2024-07-27 17:38:43 +0900

    Add emailId to success log

--- src/index.ts
+++ src/index.ts
@@ -84,13 +84,13 @@ watcher.subscribe((transaction) => {
     if (payment.amount > 0) {
       exportToMoneyForwardME(MONEY_FORWARD_EMAIL, MONEY_FORWARD_PW, payment)
         .then(() => {
-          console.log(` ✅ ${date} ${merchant} ${payment.amount}`);
+          console.log(` ✅ ${emailId} ${date} ${merchant} ${payment.amount}`);
         })
         .catch(() => {
           setTimeout(() => {
             exportToMoneyForwardME(MONEY_FORWARD_EMAIL, MONEY_FORWARD_PW, payment)
               .then(() => {
-                console.log(` ✅ ${date} ${merchant} ${payment.amount}`);
+                console.log(` ✅ ${emailId} ${date} ${merchant} ${payment.amount}`);
               })
               .catch((e) => {
                 console.log(`\n ❌ マネーフォワードへの書き出しに失敗しました。 emailId: ${emailId}\n`);
